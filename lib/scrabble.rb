5eb3b5ea12a6b01ca5e4d0e4ff00491a449fe01c
caward12 <ward.colleen.a@gmail.com>
2017-02-17 12:07:03 -0700

    through highest scoring word

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Scrabble
 
   def score(word)
@@ -17,4 +18,38 @@ class Scrabble
       "Y"=>4, "Z"=>10
     }
   end
+
+  def score_with_multipliers(word, square_values, word_multiply = 1)
+    word_array = word.upcase.chars.map! do |letter|
+        point_values[letter]
+    end
+    n=0
+    values = []
+    while n < word.length
+      values << word_array[n] * square_values[n]
+      n+=1
+    end
+    if word.length == 7
+      (values.reduce(:+)+10)*word_multiply
+    else
+      values.reduce(:+)*word_multiply
+    end
+  end
+
+  def highest_scoring_word (words)
+    highest_word = words.group_by do |word|
+      score(word)
+    end
+    if highest_word.max[1].count > 1 && highest_word.max[1].any? {|word| word.length ==7}
+      seven_letters = highest_word.max[1].find {|word| word.length ==7}
+      seven_letters
+    elsif highest_word.max[1].count > 1
+      highest_word.max[1].min_by {|word| word.length}
+    elsif highest_word.max[1].count > 1 && highest_word.max[1].all? {|word| word.length}
+      highest_word.max[1][0]
+    else
+      highest_word.max[1].join
+    end
+  end
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -1,5 +1,5 @@
 gem 'minitest'
-require_relative '../lib/scrabble'
+require './lib/scrabble'
 require 'minitest/autorun'
 require 'minitest/emoji'
 require 'pry'
@@ -8,5 +8,35 @@ class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+  end
+
+  def test_it_can_score_with_square_multipliers
+    game = Scrabble.new
+
+    assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+  end
+
+  def test_it_can_score_with_word_multipliers
+    game = Scrabble.new
+
+    assert_equal 18, game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+  end
+
+  def test_it_can_score_with_seven_word_multipliers
+    game = Scrabble.new
+
+    assert_equal 58, game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+  end
+
+  def test_it_knows_highest_scoring_word
+    game = Scrabble.new
+    game2 = Scrabble.new
+    game3 = Scrabble.new
+    game4 = Scrabble.new
+
+    assert_equal "home", game.highest_scoring_word(['home', 'word', 'hello', 'sound'])
+    assert_equal "word", game2.highest_scoring_word(['hello', 'word', 'sound'])
+    assert_equal "silence", game3.highest_scoring_word(['home', 'word', 'silence'])
+    assert_equal "word", game4.highest_scoring_word(['hi', 'word', 'ward'])
   end
 end
